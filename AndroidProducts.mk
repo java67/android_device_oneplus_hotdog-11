348b5ac7de0c0b4a5a3214ae2c11c02370a2fae5
developer <dakaizhe@yeah.net>
2020-10-13 23:38:24 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,10 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/corvus_guacamole.mk
+    $(LOCAL_DIR)/aosip_hotdog.mk
+COMMON_LUNCH_CHOICES := \
+    aosip_hotdog-userdebug
