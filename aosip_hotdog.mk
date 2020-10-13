ecf237beae7ff19f4ec5e5cc8c8f15ca487b1e69
developer <dakaizhe@yeah.net>
2020-10-13 23:47:28 +0800

    Update and rename corvus_guacamole.mk to aosip_hotdog.mk

--- aosip_hotdog.mk
+++ aosip_hotdog.mk
@@ -8,20 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from guacamole device
-$(call inherit-product, device/oneplus/guacamole/device.mk)
+# Inherit from hotdog device
+$(call inherit-product, device/oneplus/hotdog/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Inherit GApps if exsits
 $(call inherit-product-if-exists, vendor/gapps/gapps.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := corvus_guacamole
-PRODUCT_DEVICE := guacamole
+PRODUCT_NAME := aosip_hotdog
+PRODUCT_DEVICE :=hotdog
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := OnePlus 7 Pro
+PRODUCT_MODEL := HD1910
 PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_AAPT_CONFIG := xxxhdpi
@@ -33,11 +33,11 @@ TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 TARGET_BOOT_ANIMATION_RES := 1080
 # Build info
-BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/1909110008:user/release-keys"
+BUILD_FINGERPRINT := "OnePlus/OnePlus7TPro/OnePlus7Pro:10/QKQ1.190716.003/1909110008:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=OnePlus7Pro \
-    PRODUCT_NAME=OnePlus7Pro \
-    PRIVATE_BUILD_DESC="OnePlus7Pro-user 10 QKQ1.190716.003 1909110008 release-keys"
+    TARGET_DEVICE=OnePlus7TPro \
+    PRODUCT_NAME=OnePlus7TPro \
+    PRIVATE_BUILD_DESC="OnePlus7TPro-user 10 QKQ1.190716.003 1909110008 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
